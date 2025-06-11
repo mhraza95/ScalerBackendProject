58e22e3d2bbab076c7996e2f44787e47dcf1c7fb
Hassan Raza <mhraza95@gmail.com>
2025-06-11 17:00:56 +0530

    Project completed

---
+++ ScalerBackendProject-main/src/main/java/com/scaler/backendproject/service/StripePaymentGatewayImplementation.java
@@ -0,0 +1,58 @@
+package com.scaler.backendproject.service;
+
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentLink;
+import com.stripe.model.Price;
+import com.stripe.param.PaymentLinkCreateParams;
+import com.stripe.param.PriceCreateParams;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StripePaymentGatewayImplementation implements PaymentService{
+    @Override
+    public String makePayment(String orderId, Long amount) throws StripeException {
+//        return "";
+        // 1. Create PriceCreateParam Object
+        //
+
+        Stripe.apiKey = "YOUR API KEY";
+
+        PriceCreateParams params =
+                PriceCreateParams.builder()
+                        .setCurrency("INR")
+                        .setUnitAmount(amount)
+                        .setProductData(
+                                PriceCreateParams.ProductData.builder().setName(orderId).build()
+                        )
+                        .build();
+
+        Price price = Price.create(params);
+
+        //2. Creating Payment Link
+        Stripe.apiKey = "YOUR PAYMENT LINK";
+
+        PaymentLinkCreateParams linkParams =
+                PaymentLinkCreateParams.builder()
+                        .addLineItem(
+                                PaymentLinkCreateParams.LineItem.builder()
+                                        .setPrice(price.getId())
+                                        .setQuantity(1L)
+                                        .build()
+                        ).setAfterCompletion(
+                                PaymentLinkCreateParams.AfterCompletion.builder()
+                                        .setType(PaymentLinkCreateParams.AfterCompletion.Type.REDIRECT)
+                                        .setRedirect(
+                                                PaymentLinkCreateParams.AfterCompletion.Redirect.builder()
+                                                        .setUrl("https://probz.ai/")
+                                                        .build()
+                                        )
+                                        .build()
+                        )
+                        .build();
+
+        PaymentLink paymentLink = PaymentLink.create(linkParams);
+
+        return paymentLink.getId();
+    }
+}
